00f888fd54a5c68da1f8a18d95beab2b1a15f191
HubTicaBB <tijana.ilic87@protonmail.com>
2023-04-26 01:25:26 +0200

    Setup js

---
+++ session-1/javascript/fizzBuzz.test.js
@@ -0,0 +1,9 @@
+const getNumber = require("./fizzBuzz");
+
+test("Function under test doex X when Y", () => {
+  // Arrange and act
+  const result = true;
+
+  // Assert
+  expect(result).toBe(true);
+});
