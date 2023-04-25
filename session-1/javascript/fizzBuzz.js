062bba32287768ece2d674ff67814f38beaf162e
HubTicaBB <tijana.ilic87@protonmail.com>
2023-04-26 01:19:38 +0200

    Setup js

---
+++ session-1/javascript/fizzBuzz.js
@@ -0,0 +1,5 @@
+const getNumber = (number) => {
+  throw new Error("Not implemented");
+};
+
+module.exports = getNumber;
---
+++ session-1/javascript/fizzBuzz.test.js
@@ -0,0 +1,7 @@
+const getNumber = require("./fizzBuzz");
+
+describe("fizzBuzz", () => {
+  test("functionUnderTest doex X when Y", () => {
+    expect(true).toBeTruthy();
+  });
+});
