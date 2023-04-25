d1c04dde9109b11c368e10c14e7f3e141039b13f
HubTicaBB <tijana.ilic87@protonmail.com>
2023-04-25 17:37:51 +0200

    Red

--- session-1/c-sharp/Session1/Session1Tests/FizzBuzzSpecs.cs
+++ session-1/c-sharp/Session1/Session1Tests/FizzBuzzSpecs.cs
@@ -25,4 +25,20 @@ public class FizzBuzzSpecs
 
         Assert.AreEqual(expected, result);
     }
+
+    [DataTestMethod]
+    [DataRow(3)]
+    [DataRow(6)]
+    [DataRow(9)]
+    [DataRow(12)]
+    [DataRow(18)]
+    [DataRow(21)]
+    public void GetNumber_GivenMultipleOfThree_ReturnsFizz(int number)
+    {
+        var expected = "Fizz";
+
+        var result = FizzBuzz.GetNumber(number);
+
+        Assert.AreEqual(expected, result);
+    }
 }
