f605a0cda4bc425f69f1f192f9f73b32e69efc22
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2023-04-28 21:40:09 +0200

    yuuuup

--- session-1/javascript/fizzBuzz.js
+++ session-1/javascript/fizzBuzz.js
@@ -1,5 +1,36 @@
 const getNumber = (number) => {
-  throw new Error("Not implemented");
+  const firstContraint = number % 3 === 0;
+  const secondContraint = number % 5 === 0;
+  const thirdContraint = number % 7 === 0;
+  const fourthContraint = number % 11 === 0;
+
+  let returnMessage = "";
+
+  if (firstContraint) {
+    returnMessage += "Fizz";
+  }
+  if (secondContraint) {
+    returnMessage += "Buzz";
+  }
+  if (thirdContraint) {
+    returnMessage += "Whizz";
+  }
+  if (fourthContraint) {
+    returnMessage += "Bang";
+  }
+
+  if (returnMessage === "") return number;
+  return returnMessage;
 };
 
-module.exports = getNumber;
+const printNumber = (number) => {
+  if (typeof number != "number") {
+    console.log(`${number} is not a number`);
+    return;
+  }
+
+  const result = getNumber(number);
+  console.log(result);
+};
+
+module.exports = { getNumber, printNumber };
--- session-1/javascript/fizzBuzz.test.js
+++ session-1/javascript/fizzBuzz.test.js
@@ -1,9 +1,80 @@
-const getNumber = require("./fizzBuzz");
+const { getNumber, printNumber } = require("./fizzBuzz");
 
-test("Function under test doex X when Y", () => {
+/* test("Function under test doex X when Y", () => {
   // Arrange and act
   const result = true;
 
   // Assert
   expect(result).toBe(true);
+}); */
+
+const myNumbers = [[3], [6], [9]];
+test.each(myNumbers)(
+  "Function returns Fizz when number is divisible by 3",
+  (number) => {
+    // Arrange and act
+    const result = getNumber(number);
+
+    // Assert
+    expect(result).toBe("Fizz");
+  }
+);
+
+const myNumbers2 = [[5], [10], [25]];
+test.each(myNumbers2)(
+  "Function returns buzz when number is divisible by 5",
+  (myNumber) => {
+    // Arrange and act
+    const result = getNumber(myNumber);
+
+    // Assert
+    expect(result).toBe("Buzz");
+  }
+);
+
+test("Function returns FizzBuzz when number is divisible by 3 and 5", () => {
+  // Arrange and act
+  const result = getNumber(15);
+
+  // Assert
+  expect(result).toBe("FizzBuzz");
+});
+
+const myNumbers3 = [[1], [2], [4]];
+test.each(myNumbers3)(
+  "Function returns number when number is not divisible by 3 or 5",
+  (myNumber) => {
+    // Arrange and act
+    const result = getNumber(myNumber);
+
+    // Assert
+    expect(result).toBe(myNumber);
+  }
+);
+
+const myNumbers4 = [[3], [6], [9], [12], [18], [21], [24]];
+test.each(myNumbers4)("logMessage writes message to the console", (number) => {
+  const consoleMock = jest.spyOn(console, "log");
+  const expected = "Fizz";
+  printNumber(number);
+
+  expect(consoleMock).toHaveBeenCalledWith(expected);
+});
+
+test("Function returns error when number is not a number", () => {
+  const consoleMock = jest.spyOn(console, "log");
+  const expected = "a is not a number";
+
+  printNumber("a");
+
+  expect(consoleMock).toHaveBeenCalledWith(expected);
+});
+
+const myNumbers5 = [[14]];
+test.each(myNumbers5)("logMessage writes message to the console", (number) => {
+  const consoleMock = jest.spyOn(console, "log");
+  const expected = "Whizz";
+  printNumber(number);
+
+  expect(consoleMock).toHaveBeenCalledWith(expected);
 });
